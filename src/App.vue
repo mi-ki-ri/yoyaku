d41196b6ba635044c6e7bf9cb56ec5443b12db84
HOTTA Tatsuya <hotta.tatsuya.fcc@gmail.com>
2021-11-26 11:03:36 +0900

    nanka chigau

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,78 @@
 <template>
   <div id="app">
-    
+    <div class="container h-screen flex justify-start items-stretch flex-col">
+      <header class="h-10">
+        <h1 class="text-center">YOYAKU</h1>
+      </header>
+      <main class="h-screen flex justify-center items-stretch flex-row">
+        <section class="flex-grow">
+          <textarea
+            v-model="textForItems"
+            @keydown.tab="onTab"
+            class="h-screen w-full"
+          >
+          </textarea>
+        </section>
+        <section class="flex-grow-0">
+          <ul>
+            <li v-for="(task, i) in itemsFromText" :key="i">
+              {{ task }}
+            </li>
+          </ul>
+        </section>
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      textForItems: "",
+    };
+  },
+  computed: {
+    itemsFromText() {
+      const lines = this.textForItems.split("\n");
 
-}
+      const objs = []
+
+      lines.forEach((line) => {
+        const obj = {}
+        const matches = line.match(/^(\t*)([^\t]*)/i);
+        if (matches.length == 3){
+          const level = matches[1].length
+          const text = matches[2]
+          obj.level = level
+          obj.text = text
+          
+        }
+        console.log(obj)
+        objs.push(obj)
+      });
+
+      return objs;
+    },
+  },
+  methods: {
+    onTab(e) {
+      // TAB入力
+      e.preventDefault(); 
+
+      var elem = e.target;
+      var val = elem.value;
+      var pos = elem.selectionStart;
+      elem.value = val.substr(0, pos) + "\t" + val.substr(pos, val.length);
+      elem.setSelectionRange(pos + 1, pos + 1);
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
